75a009300df28243d15bfe500f4cab73fe26c39d
Benedetto Lo Giudice <bennythejudge@users.noreply.github.com>
2019-03-21 13:29:49 +0100

    Initial commit - setting up code for my own fork of the repo

--- trackerapi/me.go
+++ trackerapi/me.go
@@ -8,8 +8,8 @@ import (
 	"os"
 	u "os/user"
 
-	"github.com/GoBootcamp/clirescue/cmdutil"
-	"github.com/GoBootcamp/clirescue/user"
+	"github.com/bennythejudge/clirescue/cmdutil"
+	"github.com/bennythejudge/clirescue/user"
 )
 
 var (
